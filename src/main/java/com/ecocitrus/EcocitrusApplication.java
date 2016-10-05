58717ce9d4566aad0843b602bdc707c3a7dbe0c7
Mohamed Haidar <mohed@hotmail.com>
2016-10-05 10:41:41 +0200

    Starter project from spring

---
+++ src/main/java/com/ecocitrus/EcocitrusApplication.java
@@ -0,0 +1,12 @@
+package com.ecocitrus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcocitrusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcocitrusApplication.class, args);
+	}
+}
