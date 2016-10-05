6f2d7c2186aa0377c71681b60f24fd7966ca314d
Mohamed Haidar <mohed@hotmail.com>
2016-10-05 11:16:21 +0200

    Started on an Entity class

---
+++ src/main/java/com/ecocitrus/Invoice.java
@@ -0,0 +1,14 @@
+package com.ecocitrus;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by Administrator on 2016-10-05.
+ */
+@Entity
+public class Invoice {
+
+    protected Invoice() {
+    }
+
+}
