897d022fff1a09b789d156dd908761b658dc7fe1
Kajsa Lampa <kajsalampa@live.se>
2016-10-05 17:46:24 +0200

    more validation work2

--- src/main/java/com/ecocitrus/WebController.java
+++ src/main/java/com/ecocitrus/WebController.java
@@ -1,9 +1,9 @@
 package com.ecocitrus;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,6 +15,8 @@ import javax.validation.Valid;
 @Controller
 public class WebController {
 
+    @Autowired InvoiceRepository invoiceRepository;
+
    @GetMapping("addinvoice")
     public ModelAndView userStartPage(){
        return new ModelAndView("addInvoice")
@@ -25,14 +27,16 @@ public class WebController {
 
    @PostMapping("addinvoice")
     public ModelAndView startAndPost(@Valid Invoice invoice, BindingResult bindingResult){
-
        if (bindingResult.hasErrors()) {
            return new ModelAndView("addInvoice")
-                   .addObject("invoice", invoice);
+                   .addObject("invoice", invoice)
+                   .addObject("paymentTypes", PaymentType.values());
        }
 
+       invoiceRepository.save(invoice);
+
        return new ModelAndView("addInvoice")
-               .addObject("invoice", invoice)
+               .addObject("invoice", new Invoice())
                .addObject("paymentTypes", PaymentType.values());
 
    }
