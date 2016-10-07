5d362603971956d463839158a7d79a9b2604b7b1
Wilhelm Perdahl <wilhelm.perdahl@gmail.com>
2016-10-07 13:44:50 +0200

    Added header

---
+++ src/main/resources/static/js/header.js
@@ -0,0 +1,25 @@
+var httpRequest;
+
+makeRequest('headerIndex.html');
+
+function makeRequest(url) {
+    httpRequest = new XMLHttpRequest();
+
+    if (!httpRequest) {
+        alert('Giving up :( Cannot create an XMLHTTP instance');
+        return false;
+    }
+    httpRequest.onreadystatechange = setContents;
+    httpRequest.open('GET', url);
+    httpRequest.send();
+}
+
+function setContents() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+            $(".header").html(httpRequest.responseText);
+        } else {
+            alert('There was a problem with the request.');
+        }
+    }
+}
